ff6c63d2b334e01b5b5feb8b9633ddac0a28773b
strubloid <strubloid@gmail.com>
2022-11-28 11:36:23 +0000

    one way binded the fist data

--- frontend/src/components/posts/post.component.ts
+++ frontend/src/components/posts/post.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'post',
-  templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+    selector: 'post',
+    templateUrl: './post.component.html',
+    styleUrls: ['./post.component.scss']
 })
 
 export class PostComponent {
-  title = 'frontend';
+
+    public postData = 'No Content'
+
+    // This is the main action of a post
+    onAddPost = (postInput : HTMLTextAreaElement) => {
+
+        this.postData = postInput.value;
+
+    }
 }
