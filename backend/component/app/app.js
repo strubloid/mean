7cf04e8db7f9ecd7e17f53807a99ba0c8cf4c3fd
strubloid <strubloid@gmail.com>
2022-11-28 20:33:08 +0000

    adding basic api/posts

--- backend/component/app/app.js
+++ backend/component/app/app.js
@@ -1,15 +1,25 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
+app.use('/api/posts', (req, res, next) => {
+
+  const posts = [{
+    id: 1,
+    title: 'first post',
+    content: 'coming form the server 1',
+  },{
+      id: 2,
+      title: 'second post',
+      content: 'coming form the server 2',
+    }
+  ]
+
+  res.status(200).json({
+    message : 'success',
+    posts: posts
+  });
+
 })
 
-
-app.use((req, res, next) => {
-  res.send('Hello from Express')
-});
-
-module.exports = app;
+module.exports = app
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,76 @@
 const http = require('http');
 const app = require('./component/app/app');
-const port = process.env.PORT || 3000;
+const debug = require('debug')('node-angular');
+
+/**
+ * Make ure that the variable that we get from the
+ * environment file is set correct.
+ *
+ * @param val
+ * @returns {boolean|number|*}
+ */
+const normalizePort = val => {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)){
+    return val;
+  }
+
+  if(port >= 0){
+    return port;
+  }
+
+  return false;
+
+}
+
+/**
+ * This will be the on error main function.
+ *
+ * @param error
+ */
+const onError = error => {
+  if(error.svscall != "listen"){
+    throw error;
+  }
+
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+
+  switch (error.code) {
+
+    case "EACCESS":
+      console.error(bind + " requires elevated privileges")
+      process.exit(1)
+      break;
+
+    case "EADDRINUSE":
+      console.error(bind + " is already in use")
+      process.exit(1)
+      break;
+
+    default:
+      throw error
+  }
+}
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  debug("listening on " + port)
+}
+
+// loading the port variable + set the port
+const port = normalizePort(process.env.PORT || 3000);
+app.set('port', port)
 
 // main request
-app.set('port', port)
 const server = http.createServer(app);
 
+// starting the error handling
+server.on("error", onError);
+
+// starting the listeners
+server.on("listening", onListening);
+
+// server listening to the port
 server.listen(port);
